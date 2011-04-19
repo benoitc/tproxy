d1472f23545a215f2243266ab81cbffdaa026195
benoitc <benoitc@gmail.com>
2011-04-19 14:27:58 +0200

    bump add requirements to the archive

--- tproxy/__init__.py
+++ tproxy/__init__.py
@@ -3,5 +3,5 @@
 # This file is part of tproxy released under the MIT license. 
 # See the NOTICE for more information.
 
-version_info = (0, 2, 1)
+version_info = (0, 2, 2)
 __version__ = ".".join(map(str, version_info))
