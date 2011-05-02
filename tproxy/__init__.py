73ad741ce097659b73b79173c41ee93ec000be76
benoitc <bchesneau@gmail.com>
2011-05-02 22:12:25 +0200

    0.4.0 ssl enabled.

--- tproxy/__init__.py
+++ tproxy/__init__.py
@@ -3,5 +3,5 @@
 # This file is part of tproxy released under the MIT license. 
 # See the NOTICE for more information.
 
-version_info = (0, 3, 1)
+version_info = (0, 4, 0)
 __version__ = ".".join(map(str, version_info))
