fb570231b93f2f2425de84e6907ad1f75006810f
benoitc <bchesneau@gmail.com>
2011-05-02 05:39:45 +0200

    bump

--- tproxy/__init__.py
+++ tproxy/__init__.py
@@ -3,5 +3,5 @@
 # This file is part of tproxy released under the MIT license. 
 # See the NOTICE for more information.
 
-version_info = (0, 2, 3)
+version_info = (0, 2, 4)
 __version__ = ".".join(map(str, version_info))
